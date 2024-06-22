5afbc9bd1aa13363a71d906892f2e3d614ac282d
citraaaulia <citraauliax@gmail.com>
2024-06-22 20:08:25 +0700

    update login

--- controllers/view/views.js
+++ controllers/view/views.js
@@ -10,10 +10,6 @@ const modelPengajuanKp = require('../../models/pengajuan_kp')
 const { Op } = require('sequelize')
 const controller = {}
 
-const loginMhs = async (req, res) => {
-    res.render('login')
-}
-controller.loginMhs = loginMhs;
 
 const notifikasiMhs = async (req, res) => {
     res.render('mahasiswa/notifikasi')
@@ -30,6 +26,21 @@ const unggahBalasan = async (req, res) => {
 }
 controller.unggahBalasan = unggahBalasan;
 
+const updateProposal = async (req, res) => {
+    res.render('mahasiswa/updateProposal')
+}
+controller.updateProposal = updateProposal;
+
+const updateBalasan = async (req, res) => {
+    res.render('mahasiswa/updateBalasan')
+}
+controller.updateBalasan = updateBalasan;
+
+const login = async (req, res) => {
+    res.render('mahasiswa/login')
+}
+controller.login = login;
+
 const changePass = async (req, res) => {
     res.render('mahasiswa/changePass')
 }
@@ -39,6 +50,203 @@ const loginSekjur = async (req, res) => {
     res.render('loginSekre')
 }
 controller.loginSekjur = loginSekjur;
+
+const dataMahasiswa = async (req, res) => {
+    try {
+        const find = await modelKelompok.findAll({
+            include: [{
+                model: modelAnggota,
+                as: 'dataAnggota',
+                include: {
+
+                    model: modelMahasiswa,
+                    as: 'nama', // Alias yang sesuai dengan yang didefinisikan dalam relasi
+                    attributes: ['nama']
+                },
+                attributes: ['id_anggota', 'nim_anggota']
+            },
+            {
+                model: modelMahasiswa,
+                as: 'namaKetua', // Alias yang sesuai dengan yang didefinisikan dalam relasi
+                attributes: ['nama']
+            },
+            {
+                model: dosen,
+                as: 'namaDosen',
+                attributes: ['nama']
+            }
+            ]
+        })
+
+        const transformedData = find.map(kelompok => {
+            const transformedAnggota = [];
+
+            // Tambahkan dosen ke dalam anggota
+            if (kelompok.namaDosen) {
+                transformedAnggota.push({
+                    id_anggota: "",
+                    nim_anggota: kelompok.nip_dosen,
+                    nama: kelompok.namaDosen,
+                    status: 'Dosen'
+                });
+            }
+
+            // Tambahkan ketua ke dalam anggota
+            if (kelompok.namaKetua) {
+                transformedAnggota.push({
+                    id_anggota: "",
+                    nim_anggota: kelompok.nim_ketua,
+                    nama: kelompok.namaKetua,
+                    status: 'Ketua'
+                });
+            }
+
+            // Tambahkan anggota kelompok
+            kelompok.dataAnggota.forEach(anggota => {
+                transformedAnggota.push({
+                    id_anggota: anggota.id_anggota,
+                    nim_anggota: anggota.nim_anggota,
+                    nama: anggota.nama,
+                    status: 'Anggota'
+                });
+            });
+
+            // Return objek kelompok dengan data anggota yang telah diubah
+            return {
+                id_kelompok: kelompok.id_kelompok,
+                nim_ketua: kelompok.nim_ketua,
+                nip_dosen: kelompok.nip_dosen,
+                created_at: kelompok.created_at,
+                updated_at: kelompok.updated_at,
+                dataAnggota: transformedAnggota
+            };
+        });
+
+        return res.render('sekjur/dataMahasiswa', { data: transformedData });
+        //return res.status(200).json({ success: true, message: 'Data tersedia', data: find })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ success: false, message: 'Kesalahan Server' })
+    }
+
+}
+controller.dataMahasiswa = dataMahasiswa;
+
+const suratTugas = async (req, res) => {
+    try {
+        const findPengajuan = await modelPengajuanKp.findAll({
+            where: {
+                id_suratTugas: {
+                    [Op.ne]: null
+                }
+            },
+            include: [
+                {
+                    model: modelTugas,
+                    as: 'dataPengajuanTugas',
+
+                }
+            ],
+        })
+
+        res.render('sekjur/suratTugas', {data:findPengajuan})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ success: false, message: 'Kesalahan Server' })
+    }
+
+}
+controller.suratTugas = suratTugas;
+
+const suratTugasView = async (req, res) => {
+    try {
+        const findPengajuan = await modelTugas.findOne({
+            where: {
+                id_surat_tugas: {
+                    [Op.eq]: req.params.id
+                }
+            }
+            
+        })
+
+    
+
+        res.render('sekjur/suratTugasView', { id_tugas: req.params.id, tugas: findPengajuan })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ success: false, message: 'Kesalahan Server' })
+    }
+
+}
+controller.suratTugasView = suratTugasView;
+
+const suratPengantar = async (req, res) => {
+    try {
+        const findPengajuan = await modelPengajuanKp.findAll({
+            where: {
+                id_suratPengantar: {
+                    [Op.ne]: null
+                }
+            },
+            include: [
+                {
+                    model: modelPengantar,
+                    as: 'dataPengajuanPengantar',
+
+                }
+            ],
+        })
+        res.render('sekjur/suratPengantar', { data: findPengajuan })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ success: false, message: 'Kesalahan Server' })
+    }
+
+}
+controller.suratPengantar = suratPengantar;
+
+const suratPengantarView = async (req, res) => {
+    try {
+        const findPengajuan = await modelPengantar.findOne({
+            where: {
+                id_suratPengantar: {
+                    [Op.eq]: req.params.id
+                }
+            }
+
+        })
+
+
+
+        res.render('sekjur/suratPengantarView', { id_pengantar: req.params.id, tugas: findPengajuan })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ success: false, message: 'Kesalahan Server' })
+    }
+
+}
+controller.suratPengantarView = suratPengantarView;
+
+const tambahDosen = async (req, res) => {
+    const find = await dosen.findAll({
+    })
+    res.render('sekjur/tambahDosen', { id_kelompok: req.params.id, dataSelect: find })
+}
+controller.tambahDosen = tambahDosen;
+
+const pengajuanSuratTugas = async (req, res) => {
+    const find = await dosen.findAll({
+    })
+    res.render('sekjur/pengajuanSuratTugas', { id_kelompok: req.params.id, dataSelect: find })
+}
+controller.pengajuanSuratTugas = pengajuanSuratTugas;
+
+const pengajuanSuratPengantar = async (req, res) => {
+    const find = await dosen.findAll({
+    })
+    res.render('sekjur/pengajuanSuratPengantar', { id_kelompok: req.params.id, dataSelect: find })
+}
+controller.pengajuanSuratPengantar = pengajuanSuratPengantar;
 
 const suratBalasan = async (req, res) => {
     res.render('sekjur/suratBalasan')
